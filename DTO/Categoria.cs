b70c72c5d0230f5546d9968e3cad3c8aecfd7fe7
Roberto Basso <rob.bas.it@gmail.com>
2015-06-10 10:34:09 +0200

    removed obj dir

---
+++ DTO/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Categoria
+    {
+        public int ID { get; set; }
+        public string CategoryName { get; set; }
+
+        public Categoria() { }
+
+        public Categoria(int ID, string CategoryName) {
+            this.CategoryName = CategoryName;
+            this.ID = ID;
+        }
+    }
+}
---
+++ PointerestPortal/PointerestPortal/Controllers/CategorieController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Repositories;
+using DTO;
+
+namespace PointerestPortal.Controllers
+{
+    public class CategorieController : ApiController
+    {
+        CategorieRepository _repository;
+
+        public CategorieController()
+        {
+            _repository = new CategorieRepository();
+        }
+
+        // GET: api/Categorie
+        public IEnumerable<Categoria> GetAll()
+        {
+            return _repository.GetAll().ToList();
+        }
+
+        // GET: api/Categorie/5
+        public Categoria Get(int id)
+        {
+            return _repository.Get(id);
+        }
+
+        //// POST: api/Categorie
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT: api/Categorie/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/Categorie/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
