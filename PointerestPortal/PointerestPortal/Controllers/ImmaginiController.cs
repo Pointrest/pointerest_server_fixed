d89d8ecac6a0a59c03153997494fccd6063ab455
StefanoVuerich <vuric@hotmail.it>
2015-06-17 14:33:10 +0200

    Imagebyid added

---
+++ PointerestPortal/PointerestPortal/Controllers/ImmaginiController.cs
@@ -0,0 +1,25 @@
+﻿using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PointerestPortal.Controllers
+{
+    public class ImmaginiController : ApiController
+    {
+        ImagesRepository _rep;
+        public ImmaginiController() {
+
+            _rep = new ImagesRepository();
+        }
+
+        // GET: api/Immagini/5
+        public string Get(int id)
+        {
+            return _rep.Get(id);
+        }
+    }
+}
---
+++ Repositories/ImagesRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Repositories
+{
+    public class ImagesRepository
+    {
+        string mConnectionString;
+        private string p;
+
+        public ImagesRepository()
+            : this("connectionString")
+        {
+
+        }
+
+        public ImagesRepository(string connectionString)
+        {
+            var cs = ConfigurationManager.ConnectionStrings[connectionString];
+            if (cs == null)
+                throw new ApplicationException(string.Format("ConnectionString '{0}' not found", connectionString));
+
+            else mConnectionString = cs.ConnectionString;
+        }
+        public string Get(int id)
+        {
+            string image = null;
+            using (var connection = new SqlConnection(mConnectionString))
+            {
+                connection.Open();
+
+                string query = @"SELECT [Image]
+                                  FROM [dbo].[Immagini]
+                                  WHERE Immagini.ImmagineID = " + id;
+
+                using (var command = new System.Data.SqlClient.SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            return reader.GetValue<string>("Image");
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return null;
+        }
+    }
+}
